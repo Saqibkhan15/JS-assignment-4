f2aedeee8f011ed06cea41be737ff57c484a6b73
Saqibkhan15 <aqib12u@gmail.com>
2024-09-07 23:17:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+
+// var char = prompt("Enter a character: ");
+
+// if (char >= 48 && char <= 57) {
+//   document.write("Input is a number");
+// } else if (char >= 65 && char <= 90) {
+//   document.write("Input is an uppercase letter");
+// } else if (char >= 97 && char <= 122) {
+//   document.write("Input is a lowercase letter");
+// } else {
+//   document.write("Input is a special character");
+// }
+
+
+//q2
+// var num1 = +prompt("Enter first number:");
+// var num2 = +prompt("Enter second number:");
+
+// if (num1 > num2) {
+//   document.write("Larger number is: " + num1);
+// } else if (num1 < num2) {
+//   document.write("Larger number is: " + num2);
+// } else {
+//   document.write("Both numbers are equal");
+// }
+
+//q3
+// var num = +prompt("Enter a number:");
+
+// if (num > 0) {
+//   document.write("Number is positive");
+// } else if (num < 0) {
+//   document.write("Number is negative");
+// } else {
+//   document.write("Number is zero");
+// }
+
+//4
+// var char = prompt("Enter a character:");
+// var vowels = "aeiou";
+
+// if (vowels.indexOf(char) !== -1) {
+//   document.write("Character is a vowel");
+// } else {
+//   document.write("Character is not a vowel");
+// }
+
+//q5
+// var correctPassword = "password";
+// var userInput = prompt("Enter your password:");
+
+// if (userInput === "") {
+//   document.write("Please enter your password");
+// } else if (userInput === correctPassword) {
+//   document.write("Correct! The password you entered matches the original password");
+// } else {
+//   document.write("Incorrect password");
+// }
+
+
+//q6
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// document.write(greeting);
+
+//q7
+// var time = +prompt("Enter time in 24 hours format (HHMM):");
+
+// if (time >= 0000 && time < 1200) {
+//   document.write("Good morning");
+// } else if (time >= 12 && time < 1700) {
+//   document.write("Good afternoon");
+// } else if (time >= 17 && time < 2100) {
+//   document.write("Good evening");
+// } else if (time >= 2100 && time < 2359) {
+//   document.write("Good night");
+// }
